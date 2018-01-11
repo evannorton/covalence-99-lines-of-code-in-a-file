19b80470045aaafb908abccfbb81ef058a27c0d2
Evan Norton <evanan39@yahoo.com>
2018-01-11 12:46:37 -0600

    create button which sings on click

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
+
+//listen for button function when content loads
+document.addEventListener("DOMContentLoaded", button);
+function button() {
+    let button = document.createElement("button");
+    let btnText = document.createTextNode("Sing");
+    button.appendChild(btnText);
+    document.body.appendChild(button);
+    
+    //listen for button click
+    document.addEventListener("click", click);
+    function click() {
+        sing();
+    }
+}
+
+//friends array
 let friends = ["Brandon", "Miranda", "Cierra", "Sevaughn", "Fletcher"];
-for (let i = 0; i < friends.length; i++) {
-    for (let j = 99; j > 0; j--) {
-        if (j > 2) {
-            console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " +  (j - 1) + " lines of code in the file");
-        } else if (j === 2) {
-            console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " +  (j - 1) + " line of code in the file");
-        } else {
-            console.log(j + " line of code in the file, " + j + " line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file");
+//loop through array and log song to the console
+function sing() {
+    for (let i = 0; i < friends.length; i++) {
+        for (let j = 99; j > 0; j--) {
+            if (j > 2) {
+                console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file");
+            } else if (j === 2) {
+                console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " line of code in the file");
+            } else {
+                console.log(j + " line of code in the file, " + j + " line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file");
+            }
         }
     }
 }
